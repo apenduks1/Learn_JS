39aaa1f8cc4d7ed1b383da66415df28f893d2c1f
apenduks1 <k77154313@gmail.com>
2024-02-08 19:02:58 +0200

    feat: hw1 part 1

--- js/script.js
+++ js/script.js
@@ -1,34 +1,107 @@
-/* Створити змінну з іменем age і присвоїти їй значення свого віку. Вивести значення цієї змінної в консоль.  */
+/*'use strict'
 
-let age = 13;
-console.log(age);
 
-/* Створити змінну з іменем name і присвоїти їй значення свого імені. Вивести значення цієї змінної в консоль.  */
+const age = 18;
+console.log (typeof age)
+//number
 
-let name = 'kostya';
-console.log( name  );
+const i = 5.2;
+console.log (typeof i)
+//number
 
-/* Створити змінну з іменем isStudent і присвоїти їй значення true або false, залежно від того, чи є студентом. 
-Вивести значення цієї змінної в консоль. 
+const str = 'Hello';
+console.log (typeof str)
+//string
+
+const isFail = true;
+console.log (typeof isFail)
+//bolean
+
+const a = null;
+ //console.log (typeof a)  Помилка 
+ //null
+
+ let b;
+ console.log (typeof b)
+ //undefiend
+
+alert("When you close your information will be lose")
+
+const result = confirm("Are you here")
+console.log(result)
+
+const q = "When you close your information will be lose";
+alert(q);
+
+ const anser = 'Скільки вам років';
+ alert(anser)
+ console.log (typeof anser)
+
+ const arr =[];
+ arr[0] = prompt('Скільки вам років?' [34]);
+ arr[1] = prompt('Скільки в вас котів ?' [5]);
+ arr[2] = prompt('Яке в вас хоббі?' [Волейбол]);
+console.log(arr);
+ 
+console.tablle(date);
+const date =[];
+date[0] = prompt('Скільки вам років?' [34]);
+date[1] = prompt('Скільки в вас котів ?' [5]);
+date[2] = prompt('Яке в вас хоббі?' [Волейбол]);
+console.log(date);
+
+console.tablle({date});
+*/
+
+/*! Оголоси дві змінні, які зберігають назву та ціну товару: name і price. Присвойте змінним наступні характеристики товару (відразу при оголошенні) назва:
+ MacBook Pro 13″, ціна: 100000
+Використовуючи стандартний рядок виведи в консоль інформацію про товар, вийде: 'Обрано «MacBook Pro 13″», ціна за штуку 10000 кредитів'.
+ Присвой товару нову ціну - 20000. Використовуючи
+ стандартний рядок виведи в консоль інформацію про товар, вийде: 'Обрано «MacBook Pro 13″», ціна за штуку 2000 кредитів'. */
+
+/* const name = "MacBook Pro 13";
+ let price = 100000 ;
+
+ console.log('Обрано', name);
+ console.log('ціна за штуку',price, 'кредитів');
+
+price = 20000;
+console.log('Обрано', name);
+console.log('ціна за штуку',price, 'кредитів'); */
+
+/* Визначте змінні str, num, flag і txt зі значеннями «Привіт», 123, true, «true». За допомогою оператора визначення типу виведіть в консоль, які змінні належать типам: string, number, boolean. */
+
+/* let str = "«Привіт»";
+let num = 123;
+let flag = true;
+let txt = "«true»";
+
+console.log(typeof str)
+console.log(typeof num )
+console.log(typeof flag )
+console.log(typeof txt ) */
+
+/* За допомогою prompt запитайте ім’я користувача. Отримане значення виведіть в консоль. */
+/* 
+const q = promt('What your name','Калістратор')
+console.log()
  */
 
-let isStudent = 'true';
-console.log(isStudent);
+const a = 10;
+const b = 5;
+console.log(a % b);
+console.log(5 % 2);
+console.log(8 % 3);
+console.log(8 % 4);
 
-/* Створити змінну з іменем myString і присвоїти їй рядок з вашої улюбленою цитатою. 
- */
+console.log(8 ** 4);
+console.log(5 ** 6);
+console.log(8 ** 3);
 
-let myString = 'It is not me, it is life.';
-console.log( myString);
 
-/* Вивести цей рядок в консоль. 
-Створити змінну з іменем myNumber і присвоїти їй довільне числове значення. Потім присвоїти цій змінній результат додавання до неї числа 10. Вивести значення myNumber в консоль.  */
-
-let myNumber = 98;
-myNumber = myNumber + 10;
-console.log (myNumber);
-
-/* Створити змінну з іменем myNull і присвоїти їй значення null. Вивести значення цієї змінної в консоль. */
-
-let myNull = 'null';
-console.log (myNull)
+const x =5;
+const z =5;
+const y =10;
+console.log('x > y', x<=z);
+console.log('x === y', z===z);
+console.log('x !== y', z!==z);
