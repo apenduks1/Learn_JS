1d241446b9a49d56a3099bef3a4b1eaa3ace9ed2
apenduks1 <k77154313@gmail.com>
2024-02-15 18:27:28 +0200

    feat: dz2

--- js/script.js
+++ js/script.js
@@ -1,93 +1,6 @@
-/*'use strict'
+"use strict";
 
-
-const age = 18;
-console.log (typeof age)
-//number
-
-const i = 5.2;
-console.log (typeof i)
-//number
-
-const str = 'Hello';
-console.log (typeof str)
-//string
-
-const isFail = true;
-console.log (typeof isFail)
-//bolean
-
-const a = null;
- //console.log (typeof a)  Помилка 
- //null
-
- let b;
- console.log (typeof b)
- //undefiend
-
-alert("When you close your information will be lose")
-
-const result = confirm("Are you here")
-console.log(result)
-
-const q = "When you close your information will be lose";
-alert(q);
-
- const anser = 'Скільки вам років';
- alert(anser)
- console.log (typeof anser)
-
- const arr =[];
- arr[0] = prompt('Скільки вам років?' [34]);
- arr[1] = prompt('Скільки в вас котів ?' [5]);
- arr[2] = prompt('Яке в вас хоббі?' [Волейбол]);
-console.log(arr);
- 
-console.tablle(date);
-const date =[];
-date[0] = prompt('Скільки вам років?' [34]);
-date[1] = prompt('Скільки в вас котів ?' [5]);
-date[2] = prompt('Яке в вас хоббі?' [Волейбол]);
-console.log(date);
-
-console.tablle({date});
-*/
-
-/*! Оголоси дві змінні, які зберігають назву та ціну товару: name і price. Присвойте змінним наступні характеристики товару (відразу при оголошенні) назва:
- MacBook Pro 13″, ціна: 100000
-Використовуючи стандартний рядок виведи в консоль інформацію про товар, вийде: 'Обрано «MacBook Pro 13″», ціна за штуку 10000 кредитів'.
- Присвой товару нову ціну - 20000. Використовуючи
- стандартний рядок виведи в консоль інформацію про товар, вийде: 'Обрано «MacBook Pro 13″», ціна за штуку 2000 кредитів'. */
-
-/* const name = "MacBook Pro 13";
- let price = 100000 ;
-
- console.log('Обрано', name);
- console.log('ціна за штуку',price, 'кредитів');
-
-price = 20000;
-console.log('Обрано', name);
-console.log('ціна за штуку',price, 'кредитів'); */
-
-/* Визначте змінні str, num, flag і txt зі значеннями «Привіт», 123, true, «true». За допомогою оператора визначення типу виведіть в консоль, які змінні належать типам: string, number, boolean. */
-
-/* let str = "«Привіт»";
-let num = 123;
-let flag = true;
-let txt = "«true»";
-
-console.log(typeof str)
-console.log(typeof num )
-console.log(typeof flag )
-console.log(typeof txt ) */
-
-/* За допомогою prompt запитайте ім’я користувача. Отримане значення виведіть в консоль. */
-/* 
-const q = promt('What your name','Калістратор')
-console.log()
- */
-
-const a = 10;
+/* const a = 10;
 const b = 5;
 console.log(a % b);
 console.log(5 % 2);
@@ -98,10 +11,64 @@ console.log(8 ** 4);
 console.log(5 ** 6);
 console.log(8 ** 3);
 
+const x = 5;
+const z = 5;
+const y = 10;
+console.log("x > y", x <= z);
+console.log("x === y", z === z);
+console.log("x !== y", z !== z);
+ */
+/*  
+let vealue = '45';
+console.log (typeof vealue);
+vealue = Number(vealue);
+console.log (typeof vealue);
 
-const x =5;
-const z =5;
-const y =10;
-console.log('x > y', x<=z);
-console.log('x === y', z===z);
-console.log('x !== y', z!==z);
+const data = promt("how old  are you", '45')
+ */
+
+/* 
+
+console.log ( data)
+console.log (typeof data)
+ */
+
+/* let t = "18";
+console.log (Number(t))
+console.log (+t)
+/* Number =+ */
+/* 
+
+
+const isFail = false;
+console.log = (+isFail);
+ */
+
+/* const apples =50;
+const oranges ='20.12ufHSDUcbvh';
+console.log (apples + oranges);
+console.log (+apples + +oranges) ;
+ */
+
+/* const apples =50;
+const oranges ='20.12ufHSDUcbvh';
+ console.log(parseInt(apples));
+ console.log(parseFloat(oranges));
+ */
+
+/* const apples = 50;
+const oranges = "20.12ufHSDUcbvh";
+console.log(isNaN(oranges));
+console.log(isNaN(apples));
+ */
+
+console.log(Math.floor(2.9));//2
+console.log(Math.ceil(3.4));//4
+console.log(Math.round(5.7));//6
+console.log(Math.round(5.3));//5
+console.log(Math.max(9, 2, 7 , 4));//9
+console.log(Math.min(9, 2, 7 , 4));//2
+console.log(Math.random());
+console.log((Math.random() * (100 - 50 ) +1) .toFixed()) ;
+
+
