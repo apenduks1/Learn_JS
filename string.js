36e670c6cb540de3384006c7054095acd2bff9d9
apenduks1 <k77154313@gmail.com>
2024-02-18 17:59:43 +0200

    feat:hw 3 part 1

---
+++ string.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const message = "Mango" + "is" + "happy";
+console.log(message);
+
+let result;
+
+result = 5 + "5";
+console.log(result);
+console.log(typeof result);
+
+result = 5 + "5" + 5;
+console.log(result);
+console.log(typeof result);
+
+
+
+result = 5/ "2"
+console.log(result);
+console.log(typeof result);
+console.log (str.indexOf("aga"))
